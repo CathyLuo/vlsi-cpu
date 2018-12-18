cfb3a6cec5777f84bdbdfb105b65c8670c91a993
CathyLuo <catherineluokey@outlook.com>
2018-12-18 00:48:18 -0500

    sram renew

--- src/candy_sram.v
+++ src/candy_sram.v
@@ -1,37 +1,42 @@
 `include "candy_defines.v"
 
 module candy_sram(
-    input wire clk, 
-	input wire rst,
+    input wire clk,
+    input wire rst,
 
-	input wire write_enable,
-	input wire [`SRAMAddrWidth] waddr,
-	input wire [`SRAMDataWidth] wdata,
+    input wire write_enable_i,
+    input wire [`SRAMAddrWidth] sram_waddr_i,
+    input wire [`SRAMDataWidth] sram_wdata_i,
 
-	input wire read_enable,
-	input wire [`SRAMAddrWidth] raddr,
-	output reg [`SRAMDataWidth] rdata,
-    output reg rdata_ready
+    input wire read_enable_i,
+    input wire [`SRAMAddrWidth] sram_raddr_i,
+    output reg [`SRAMDataWidth] sram_rdata_i,
+
+    output reg write_enable_o,
+    output reg read_enable_o,
+    inout [`SRAMDataWidth] sram_data_io,
+    output reg chip_enable_o
 );
 
-reg [`SRAMDataWidth] SRAM [`SRAMAddrWidth];
+assign sram_data_io = (!read_enable_i && write_enable_i) ? sram_waddr_i : 24'bz;
 
-initial $readmemh ("sram.data", SRAM);
-
-always @ (posedge clk) begin
-	if (read_enable && !write_enable) begin
-		rdata <= SRAM[raddr];
-        rdata_ready <= `ReadReady;
-	end
-    if(rdata_ready == `ReadReady) begin
-        rdata_ready <= `ReadNotReady;
+always @(posedge clk) begin
+    if(read_enable_i ^ write_enable_i) begin
+        if(read_enable_i) begin
+            chip_enable_o <= 1'b0;
+            read_enable_o <= 1'b0;
+            write_enable_o <= 1'b1;
+            sram_rdata_i <= sram_data_io;
+        end
+        if(write_enable_i) begin
+            chip_enable_o <= 1'b0;
+            write_enable_o <= 1'b0;
+            read_enable_o <= 1'b1;
+        end
     end
-end
-
-always @ (posedge clk) begin
-	if(write_enable && read_enable) begin
-		SRAM[waddr] <= wdata;
-	end
-end
-
-endmodule 
+    else
+        chip_enable_o <= 1'b1;
+        write_enable_o <= 1'b1;
+        read_enable_o <= 1'b1;
+    end
+endmodule
